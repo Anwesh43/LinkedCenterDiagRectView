91a9b4176d590121e1fca6520539ed8d63f10275
Anwesh43 <anweshthecool0@gmail.com>
2019-04-24 09:03:16 +0530

    adding drawCDRNode canvas extension method

--- centerdiagrectview/src/main/java/com/anwesh/uiprojects/centerdiagrectview/CenterDiagRectView.kt
+++ centerdiagrectview/src/main/java/com/anwesh/uiprojects/centerdiagrectview/CenterDiagRectView.kt
@@ -21,6 +21,7 @@ val strokeFactor : Int = 90
 val sizeFactor : Float = 2.9f
 val foreColor : Int = Color.parseColor("#4CAF50")
 val backColor : Int = Color.parseColor("#BDBDBD")
+val rectHFactor : Float = 4f
 
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n)
@@ -32,3 +33,30 @@ fun Float.mirrorValue(a : Int, b : Int) : Float {
 }
 fun Float.updateValue(dir : Float, a : Int, b : Int) : Float = mirrorValue(a, b) * dir * scGap
 
+fun Canvas.drawCenterDiag(x : Float, y : Float, sc : Float, paint : Paint) {
+    drawLine(0f, 0f, x * sc, y * sc, paint)
+}
+
+fun Canvas.drawCDRNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    val gap : Float = h / (nodes + 1)
+    val size : Float = gap / sizeFactor
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    paint.color = foreColor
+    paint.style = Paint.Style.STROKE 
+    val sc1 : Float = scale.divideScale(0, 2)
+    val sc2 : Float = scale.divideScale(1, 2)
+    save()
+    translate(w / 2, gap * (i + 1))
+    rotate(90f * sc2)
+    drawRect(RectF(-size, -size / rectHFactor, size, size / rectHFactor), paint)
+    for (j in 0..(lines - 1)) {
+        save()
+        rotate(90f * j)
+        drawCenterDiag(-size, -size / rectHFactor, sc1.divideScale(j, lines), paint)
+        restore()
+    }
+    restore()
+}
