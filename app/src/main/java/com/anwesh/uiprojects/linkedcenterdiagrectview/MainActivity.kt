3b80d3ba6c529c4b97d06fb5884465a108fb7bc8
Anwesh43 <anweshthecool0@gmail.com>
2019-04-24 09:23:22 +0530

    adding view in activity

--- app/src/main/java/com/anwesh/uiprojects/linkedcenterdiagrectview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedcenterdiagrectview/MainActivity.kt
@@ -2,11 +2,12 @@ package com.anwesh.uiprojects.linkedcenterdiagrectview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.centerdiagrectview.CenterDiagRectView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        CenterDiagRectView.create(this)
     }
 }
